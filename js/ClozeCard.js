2204b303f7e200327fed3e8c2dde6eaa5d723681
Drew Mercer <japanexpress@gmail.com>
2017-09-10 20:14:27 -0400

    Updating cloze logic

--- js/ClozeCard.js
+++ js/ClozeCard.js
@@ -2,3 +2,19 @@ var fs = require('fs');
 
 module.exports = ClozeFlash;
 
+function ClozeFlash(text, cloze) {
+    this.text = text;
+    cloze = this.cloze;
+    this.clozeDelete = this.text.replace(this.cloze, '_______');
+    this.create = function() {
+        var data = {
+            text: this.text,
+            cloze: this.cloze,
+            clozeDelete: this.clozeDelete,
+            type: 'cloze'
+        };
+        fs.appendFile('log.txt', JSON.stringify(data) + ';', 'utf8', function(error) {
+                if (error) throw error;
+            }
+        }
+    }
