0cfe4a55f2a1d6bf60a2deac1528aff3056651db
Drew Mercer <japanexpress@gmail.com>
2017-09-13 07:48:29 -0400

    Working on exception bug

--- js/BasicCard.js
+++ js/BasicCard.js
@@ -19,7 +19,8 @@ function BasicCard(front, back) {
 var george = BasicCard("Who was the president (the first one from 1789-1797) who lived in Mount Vernon?", "Washington"),
     andrew = BasicCard("What president succeeded Abraham Lincoln when he was assassinated?", "Johnson"),
     brown = BasicCard("Whose raid on Harper's Ferry led to further tensions before the war?", "Brown"),
-    phila = BasicCard(" In what city was the Declaration of Independence signed?", "Philadelphia");
+    phila = BasicCard("In what city was the Declaration of Independence signed?", "Philadelphia");
+    trump = BasicCard("Who is the current President of the US?", "Trump");
 
 fullBasicArray.push(frontArray);
 fullBasicArray.push(backArray);
@@ -27,5 +28,7 @@ george.push();
 andrew.push();
 brown.push();
 phila.push();
+trump.push();
 
-module.exports = fullBasicArray;
+// module.exports = fullBasicArray;
+module.exports = BasicCard;
